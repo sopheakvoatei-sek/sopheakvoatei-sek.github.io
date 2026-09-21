ee781e3599d071ebf1189861cc3f6b8963d29cc5
SekSopheakVoatei <sopheakvoateisek@gmail.com>
2026-09-21 14:49:58 +0200

    fix css

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-const repo = "sopheakvoatei-seksopheakvoatei-sek.github.io";
 
 const nextConfig = {
   output: "export",
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
   trailingSlash: true,
 
   images: {
